a686aeb73cdbb7f59b6b66664fbab24c7b74b12f
ncontrerasn <ncontrerasn@unal.edu.co>
2020-12-05 20:52:41 -0500

    dependencias

--- AFD/src/com/company/Main.java
+++ AFD/src/com/company/Main.java
@@ -5,11 +5,10 @@ import org.antlr.v4.runtime.tree.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        GrammarAFDLexer lexer = new MiLenguajeLexer(CharStreams.fromFileName("input.txt"));
+        GrammarAFDLexer lexer = new GrammarAFDLexer(CharStreams.fromFileName("input.txt"));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
-        MiLenguajeParser parser = new MiLenguajeParser(tokens);
-        ParseTree tree = parser.prog();
-
+        GrammarAFDParser parser = new GrammarAFDParser(tokens);
+        ParseTree tree = parser.automata();
         MyVisitor<Object> loader = new MyVisitor<Object>();
         loader.visit(tree);
     }
